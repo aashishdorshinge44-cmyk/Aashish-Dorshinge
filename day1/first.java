d7eb45cf0cc7aaa7fee5a5faa69d870d513d4384
aashishdorshinge44-cmyk <aashishdorshinge44@gmail.com>
2025-09-24 01:10:26 +0530

    Add files via upload
    Day 1: My Java Learning Journey
    
    Today I started learning Java programming.
    
    I learned about data types in Java, which are used to define the type of data a variable can hold (like int, float, char, etc.).
    
    I practiced writing a Java program with System.out.println() (soup → you meant System.out.println() 😊) to display messages on the screen.
    
    I also understood how to assign values to constants using the final keyword in Java.
    
    It was my first step, and I feel excited to explore more about Java programming in the coming days! 🚀

---
+++ day1/first.java
@@ -0,0 +1,9 @@
+public class first {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+      System.out.println("A IS : " + a );
+      System.out.println("B IS "+ b  );
+
+    }
+}
---
+++ day1/second.java
@@ -0,0 +1,19 @@
+public class second {
+    public static void main(String[] args) {
+        byte b = 8;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        
+        boolean var = false;
+        float price = 10.5F;
+        int number = 25;
+        short n = 240;
+        
+        System.out.println("price is : "+ price);
+        System.out.println("int num = "+ number);
+        System.out.println("n is "+ n );
+
+    }
+    
+}
