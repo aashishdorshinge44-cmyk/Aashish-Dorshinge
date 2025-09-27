e563331a43ef4902737eb90eec88aafdb4567f96
aashishdorshinge44-cmyk <aashishdorshinge44@gmail.com>
2025-09-28 02:38:16 +0530

    Add files via upload
    .
    
    📌 Day 5 – Type Casting in Java
    🔹 What is Type Casting?
    
    Type casting in Java is the process of converting one data type into another. It helps us to use values in different forms when needed.
    
    For example, sometimes we need to change a smaller data type into a bigger one (like int to double), or a bigger one into a smaller one (like double to int).

---
+++ day 5/tenth.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class tenth {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float marks = 99.999f;
+        int number = (int)marks;
+        System.out.println(number);
+        
+    }
+    
+}
