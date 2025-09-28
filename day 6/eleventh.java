40104647872f71064fdd40e4aa7361290d35cd62
Aashish Dorshinge <aashishdorshinge44@gmail.com>
2025-09-28 23:20:43 +0530

    Add files via upload
    :
    
    📘 Day 6 – Type Promotion in Expressions (Java)
    
    In Java, type promotion means automatically converting smaller data types into larger data types when evaluating expressions. This ensures accuracy and prevents data loss during calculations.
    
    🔹1) Key Rules of Type Promotion in Java:
    
    Byte, short, and char values are promoted to int when used in an expression.
    Example:
    
    byte a = 10, b = 20;
    byte c = (byte)(a + b); // a+b promoted to int before addition
    
    
    2) If one operand is long, the whole expression is promoted to long.
    
    3) If one operand is float, the whole expression is promoted to float.
    
    4) If one operand is double, the whole expression is promoted to double

---
+++ day 6/eleventh.java
@@ -0,0 +1,9 @@
+public class eleventh {
+public static void main(String[] args) {
+    int a = 10;
+    float b = 20.25f;
+    long c = 30;
+    double ans = a + b + c;
+    System.out.println(ans);
+}    
+}
---
+++ day 6/twelve.java
@@ -0,0 +1,11 @@
+public class twelve {
+    public static void main(String[] args) {
+        char ch = 'A';
+        char ch2 = 'B';
+        int number = ch;
+        int number2 = ch2;
+        System.out.println(number);
+        System.out.println(number2);
+    }
+    
+}
