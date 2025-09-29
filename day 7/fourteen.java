f8ef2d5182876ce76ffbf1c7a259bbdac3d37917
Aashish Dorshinge <aashishdorshinge44@gmail.com>
2025-09-29 22:34:28 +0530

    Add files via upload
    Day 7 – Java Learning
    📘 Today I learned:
    
    1) How to take three numbers (a, b, c) as input and calculate their average.
    
    2) How to take the side of a square as input and calculate its area.
    
    Step by step, building stronger basics in Java! 🚀

---
+++ day 7/fourteen.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class fourteen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the side length of the square:");
+        int side = sc.nextInt();
+        int area = side * side;
+        System.out.println(area);
+    }
+}
