5b8557b1ca92186181957b59bb15958746a84867
Aashish Dorshinge <aashishdorshinge44@gmail.com>
2025-10-01 01:46:15 +0530

    Add files via upload
    📌 Day 8 – Operators in Java
    
    Today, I learned about operators in Java.
    Operators are special symbols that are used to perform operations on variables and values. They help us manipulate data, perform calculations, compare values, and control logic in a program.

---
+++ day 8/fifthteen.java
@@ -0,0 +1,9 @@
+public class fifthteen {
+    public static void main(String[] args) {
+    int A = 5;
+    int B = 10;
+    System.out.println("addition"+( A + B ));    
+    
+    }
+    
+}
---
+++ day 8/sixteen.java
@@ -0,0 +1,12 @@
+public class sixteen {
+    public static void main(String[] args) {
+        int A = 5;
+        int B = 10;
+        System.out.println("multiplication"+( A * B ));
+        System.out.println("subtraction"+( B - A ));
+        System.out.println("division"+( B / A ));
+        System.out.println("modulus"+( B % A )) ;   
+        
+        }
+    
+}
