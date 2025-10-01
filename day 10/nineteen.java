6a9fe2f3599cb893b48a6f57510b8e7b502bed77
Aashish Dorshinge <aashishdorshinge44@gmail.com>
2025-10-02 03:28:08 +0530

    Add files via upload
    Day 10 – Java Learning
    Today I learned about pre-increment (++a), post-increment (a++), pre-decrement (--a), and post-decrement (a--).
    
    Pre-operators change the value first, then use it.
    
    Post-operators use the value first, then change it.

---
+++ day 10/nineteen.java
@@ -0,0 +1,8 @@
+public class nineteen {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = --A;
+        System.out.println(A);
+        System.out.println(B);
+    }
+}
